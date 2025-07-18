148bd0f968a8b3bad9621481942faf9b91fb7f34
nikhildevnikhil <nikhildeonikhil@gmail.com>
2025-07-18 23:20:16 +0530

    1.0

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,10 +8,10 @@ import { Fira_Code } from 'next/font/google'
 
 const firaCode = Fira_Code({ subsets: ['latin'], weight: ['300', '400', '500', '600', '700'] })
 
-const title = 'John Doe | Full-Stack Web Developer in Chicago'
+const title = 'Nikhil Deo | Full-Stack Web Developer'
 
 const description =
-  "Skilled full-stack web developer in Chicago. I build responsive, user-friendly websites with React, NextJS, and NodeJS. Let's bring your vision to life. Hire me today!"
+  "Skilled full-stack web developer with expertise in React, SpringBoot. Let's bring your vision to life. Hire me today!"
 
 const url = process.env.NEXT_PUBLIC_SITE_URL
 
