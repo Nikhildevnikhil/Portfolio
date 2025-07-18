0d4c95338d99d65390f6955a23a933472a4013aa
nikhildevnikhil <nikhildeonikhil@gmail.com>
2025-07-18 23:42:42 +0530

    2.0

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 
   images: {
     remotePatterns: [
